4fda4879e550297a72628a1a4144618817433f4f
gompf01 <gompf01@users.noreply.github.com>
2020-05-01 16:30:21 +0200

    Add files via upload

---
+++ ScanDVB-C.sh
@@ -0,0 +1,36 @@
+./octoscan --freq=122 --msys=dvbc --sr=6900 --mtype=64qam  --create Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=330 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=338 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=346 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=354 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=362 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=370 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=378 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=386 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=394 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=402 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=410 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=418 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=426 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=434 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=442 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=450 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=458 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=466 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=474 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=482 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=490 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=498 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=522 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=530 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=538 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=546 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=554 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=562 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=570 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=578 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=586 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=594 --msys=dvbc --sr=6900 --mtype=256qam --append Scanned_DVB-C.m3u 172.16.15.99
+./octoscan --freq=610 --msys=dvbc --sr=6900 --mtype=64qam  --append Scanned_DVB-C.m3u 172.16.15.99
+
+
---
+++ ScanDVB-T.sh
@@ -0,0 +1,8 @@
+./octoscan --freq=514 --msys=dvbt --create Scanned_DVB-T.m3u 172.16.15.99
+./octoscan --freq=546 --msys=dvbt --append Scanned_DVB-T.m3u 172.16.15.99
+./octoscan --freq=554 --msys=dvbt --append Scanned_DVB-T.m3u 172.16.15.99
+./octoscan --freq=578 --msys=dvbt --append Scanned_DVB-T.m3u 172.16.15.99
+./octoscan --freq=586 --msys=dvbt --append Scanned_DVB-T.m3u 172.16.15.99
+./octoscan --freq=690 --msys=dvbt --append Scanned_DVB-T.m3u 172.16.15.99
+
+
