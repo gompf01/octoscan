fd0d84a5e7045ed7c6e1dcff6e5dda8a55083326
gompf01 <gompf01@users.noreply.github.com>
2020-06-20 16:19:30 +0200

    Update ScanDVB-T.sh
    Added a Sleep which has proven to make ist much more stable, which results (for me at least) to get all transponders scanned.
    
    Changed from IP address to "sat.ip" which should work for most  products according to the spec ;)  Anyway my IP should be useless for everyone else

--- ScanDVB-T.sh
+++ ScanDVB-T.sh
@@ -1,8 +1,13 @@
-./octoscan --freq=514 --msys=dvbt --create Scanned_DVB-T.m3u 172.16.15.99
-./octoscan --freq=546 --msys=dvbt --append Scanned_DVB-T.m3u 172.16.15.99
-./octoscan --freq=554 --msys=dvbt --append Scanned_DVB-T.m3u 172.16.15.99
-./octoscan --freq=578 --msys=dvbt --append Scanned_DVB-T.m3u 172.16.15.99
-./octoscan --freq=586 --msys=dvbt --append Scanned_DVB-T.m3u 172.16.15.99
-./octoscan --freq=690 --msys=dvbt --append Scanned_DVB-T.m3u 172.16.15.99
+./octoscan --freq=514 --msys=dvbt --create Scanned_DVB-T.m3u sat.ip
+sleep 15
+./octoscan --freq=546 --msys=dvbt --append Scanned_DVB-T.m3u sat.ip
+sleep 15
+./octoscan --freq=554 --msys=dvbt --append Scanned_DVB-T.m3u sat.ip
+sleep 15
+./octoscan --freq=578 --msys=dvbt --append Scanned_DVB-T.m3u sat.ip
+sleep 15
+./octoscan --freq=586 --msys=dvbt --append Scanned_DVB-T.m3u sat.ip
+sleep 15
+./octoscan --freq=690 --msys=dvbt --append Scanned_DVB-T.m3u sat.ip
 
 
